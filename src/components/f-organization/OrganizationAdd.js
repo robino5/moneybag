3931d0600e066446eb8f3eaf84e88a0fb2be8bd9
robino5 <anamulrobin05@gmail.com>
2022-09-14 10:52:56 +0600

    add from validation

--- src/components/f-organization/OrganizationAdd.js
+++ src/components/f-organization/OrganizationAdd.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import {
     CCard,
@@ -16,6 +16,19 @@ import {
 
 
 const OrganizationAdd = () => {
+    const [validated, setValidated] = useState(false)
+    const saveOrganization = (event) => {
+        const form = event.currentTarget
+        if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        setValidated(true)
+        if(validated){
+            alert("everything is ok")
+        }
+    }
+    
     return (
         <div className="bg-light min-vh-100 d-flex flex-row">
             <CContainer>
@@ -23,17 +36,24 @@ const OrganizationAdd = () => {
                     <CCol md={8}>
                         <CCard className="p-4">
                             <CCardBody>
-                                <CForm>
+                                <CForm noValidate
+                                    validated={validated}>
                                     <CRow className="mb-3">
                                         <CFormLabel className="col-sm-3 col-form-label">Name</CFormLabel>
                                         <CCol sm={9}>
-                                            <CFormInput type="text" placeholder="Organization Name" />
+                                            <CFormInput type="text" 
+                                            placeholder="Organization Name"  
+                                            feedbackInvalid="Please provide a valid zip." 
+                                            required/>
                                         </CCol>
                                     </CRow>
                                     <CRow className="mb-3">
                                         <CFormLabel className="col-sm-3 col-form-label">Short Name</CFormLabel>
                                         <CCol sm={9}>
-                                            <CFormInput type="text" placeholder="Organization Short Name" />
+                                            <CFormInput type="text"  
+                                            feedbackInvalid="Please provide Organization Name" 
+                                            placeholder="Organization Short Name" 
+                                            required/>
                                         </CCol>
                                     </CRow>
                                     <CRow className="mb-3">
@@ -45,14 +65,14 @@ const OrganizationAdd = () => {
                                     <CRow className="mb-3">
                                         <CFormLabel className="col-sm-3 col-form-label">Status</CFormLabel>
                                         <CCol sm={9}>
-                                        <CFormCheck  label="Active"/>
+                                            <CFormCheck label="Active" />
                                         </CCol>
                                     </CRow>
                                     <div className="text-center ">
-                                    <Link to="/orgnization">
-                                    <CButton color="danger" className="mx-3" >Cancle</CButton>
-                                    </Link>
-                                    <CButton color="success" >Save</CButton>
+                                        <Link to="/orgnization">
+                                            <CButton color="danger" className="mx-3" >Cancle</CButton>
+                                        </Link>
+                                        <CButton type="submit" color="success" onClick={saveOrganization} >Save</CButton>
                                     </div>
                                 </CForm>
                             </CCardBody>
